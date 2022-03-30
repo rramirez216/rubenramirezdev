204e9a56032d576ba8e9b0e4ad8544a8aadd75d8
Ruben Ramirez <rramirez110911@gmail.com>
2022-03-29 20:42:18 -0700

    turns the children of the nav bar into a row and adds a logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,18 +3,31 @@ import styled from 'styled-components'
 
 const Navigation = () => {
   return (
-    <nav>
-      <Name>Ruben Ramirez</Name>
-      <div>About</div>
-      <div>Projects</div>
-      <div>Contact</div>
-    </nav>
+    <Nav>
+      <Side>
+        <Logo>Ruben Ramirez</Logo>
+      </Side>
+      <NavLink href='#'>About</NavLink>
+      <NavLink href='#'>Projects</NavLink>
+      <NavLink href='#'>Contact</NavLink>
+      <Side />
+    </Nav>
   )
 }
 
-const Name = styled.div`
+const Nav = styled.nav`
+  display: flex;
+  flex-flow: row nowrap;
+`
+const Side = styled.div`
+  flex: 1;
+`
+const Logo = styled.div`
   font-weight: 900;
   font-size: 2rem;
+`
+const NavLink = styled.a`
+  display: block;
 `
 
 export default Navigation
