f8d1ecbd51362781d31871293f75796d7e483f13
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-09 23:25:47 -0700

    adds icons and fixes to the layout

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import GlobalStyles from './components/globalstyles/GlobalStyles'
 import Navigation from './components/Nav'
 // image
 import Portrait from './Assets/images/portrait.JPG'
+// SVG Icons
+import { Linkedin, GitHub, Mail } from 'react-feather'
 
 const App = () => {
   // const [copied, setCopied] = useState(false)
@@ -20,9 +22,30 @@ const App = () => {
     <Wrapper>
       <Navigation isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
       <Main>
-        <ImageWrapper>
-          <Image src={Portrait} alt='portrait' />
-        </ImageWrapper>
+        <ImageAndLinkContainer>
+          <ImageWrapper>
+            <Image src={Portrait} alt='portrait' />
+          </ImageWrapper>
+          <LinkWrapper>
+            <Link href=''>
+              <Mail />
+            </Link>
+            <Link href=''>
+              <Linkedin />
+            </Link>
+            <Link href=''>
+              <GitHub />
+            </Link>
+          </LinkWrapper>
+        </ImageAndLinkContainer>
+        <About>
+          {/* <h1>About</h1> */}
+          <p>
+            Hi there! I'm Ruben Ramirez, a self-motivated Frontend Web Developer
+            based in San Diego, CA. I have a passion for learning new things,
+            solving problems, and bringing joy to others through code.
+          </p>
+        </About>
       </Main>
       <GlobalStyles />
     </Wrapper>
@@ -35,6 +58,14 @@ const Wrapper = styled.div`
 const Main = styled.main`
   display: flex;
   flex-flow: column nowrap;
+  padding: 0 16px;
+`
+const ImageAndLinkContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
 `
 const ImageWrapper = styled.div`
   width: 150px;
@@ -45,6 +76,19 @@ const ImageWrapper = styled.div`
 const Image = styled.img`
   display: block;
   /* width: 100%; */
+`
+const LinkWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`
+const Link = styled.a`
+  display: block;
+`
+const About = styled.div`
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export default App
