3021a00c9351b5be06e8c91a8ac252f30fdc683e
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-11 18:40:27 -0700

    creates new component for a list of technical skills

---
+++ src/components/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Skills = () => {
+  return (
+    <Wrapper>
+      <p>Technologies I work with:</p>
+      <ul>
+        <li>React</li>
+      </ul>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+export default Skills
