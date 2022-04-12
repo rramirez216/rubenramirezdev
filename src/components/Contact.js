55fb29f2499918f2d48a5d1c88bd478c574f8c68
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-11 20:05:55 -0700

    creates new components for contact info and a list of projects

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Contact = () => {
+  return (
+    <Wrapper>
+      <div>
+        <p>contact info here</p>
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+export default Contact
---
+++ src/components/ProjectList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ProjectList = () => {
+  return (
+    <Wrapper>
+      <div>This</div>
+      <div>Is for a list of Projets</div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  display: flex;
+`
+export default ProjectList
