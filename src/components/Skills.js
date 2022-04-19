f98b5a5e25cf3ab5b9a0c2a49d778fc0cc4951ba
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-18 20:38:00 -0700

    changes to the skills section
    -bullet element was moved up to be a sibling of the skill
    
    LiContainer was created to animate it on hover
    
    adds variants for the list item bullet and licontainer that is actived while hovering over the list item

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 import { skills } from '../data/data'
 
 const Skills = () => {
+  const listItemVariant = {
+    rest: {
+      x: 0,
+    },
+    hover: {
+      x: 100,
+    },
+  }
+  const bulletVariant = {
+    rest: {
+      // opacity: 0,
+      x: 0,
+    },
+    hover: {
+      x: 100,
+      // opacity: 1,
+      transition: {
+        delay: 0.1,
+        duration: 0.2,
+      },
+    },
+  }
   return (
     <Wrapper>
       <Heading>Technologies I've worked with:</Heading>
       <List>
         {skills.map((skill, index) => (
-          <ListItem key={index}>
-            <Bullet></Bullet>
-            {skill}
+          <ListItem
+            initial='rest'
+            whileHover='hover'
+            animate='rest'
+            key={index}
+          >
+            <Bullet variants={bulletVariant}></Bullet>
+            <LiContainer variants={listItemVariant}>{skill}</LiContainer>
           </ListItem>
         ))}
       </List>
@@ -28,20 +56,25 @@ const Heading = styled.h2`
   width: 100%;
 `
 const List = styled.ul`
+  display: flex;
+  flex-flow: column;
+  gap: 8px;
   list-style: none;
   margin: 0;
   padding: 0;
 `
-const ListItem = styled.li`
+const ListItem = styled(motion.li)`
   display: flex;
   align-items: baseline;
-  gap: 16px;
+  gap: 8px;
 `
-const Bullet = styled.span`
+const LiContainer = styled(motion.div)``
+const Bullet = styled(motion.span)`
   display: block;
   width: 16px;
   height: 16px;
-  border: 4px solid hsl(138, 81%, 29%);
+  /* border: 2px solid hsl(0, 0%, 25%); */
+  border: 2px solid hsl(138, 81%, 29%);
   /* background-color: hsl(138, 81%, 29%); */
   border-radius: 50%;
 `
