d92d7186891279abb7355922945cf931392eff3f
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-19 21:04:17 -0700

    adds new props and links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Project = ({ title, summary }) => {
+const Project = ({ title, description, stack, repo, demo }) => {
   return (
     <Wrapper>
       <Title>{title} -</Title>
-      <p>{summary}</p>
+      <p>{description}</p>
+      <p>Tech Stack: {stack}</p>
+      <div>
+        <a href={repo}>view repo</a>
+        <a href={demo}>live demo</a>
+      </div>
     </Wrapper>
   )
 }
