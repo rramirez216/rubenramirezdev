48a1518ddf528aa08b2153dcc40b93ec516965c0
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-21 18:23:28 -0700

    removes extra spaces and commas inside of the stack array of each object in the project Array

--- src/data/data.js
+++ src/data/data.js
@@ -4,7 +4,7 @@ const projectsArr = [
     title: 'Random Champion Picker',
     description:
       'A tool created to help pick a random League of Legends champion for the user to play.',
-    stack: ['CSS,  ', 'Styled Components  ', 'JavaScript,  ', 'ReactJS'],
+    stack: ['CSS', 'Styled Components', 'JavaScript', 'ReactJS'],
     repo: 'https://github.com/rramirez216/random-champion-picker',
     demo: 'https://randomchampionpicker.netlify.app/',
   },
@@ -13,7 +13,7 @@ const projectsArr = [
     title: 'SALVAMEX',
     description:
       'A responsive brochure website promoting the services, and showcasing the work provided by the construction company SALVAMEX.',
-    stack: ['TailwindCSS  , ', 'JavaScript  , ', 'HTML5'],
+    stack: ['TailwindCSS', 'JavaScript', 'HTML5'],
     repo: 'https://github.com/rramirez216/Salvamex',
     demo: 'https://salvamexconstruction.com/',
   },
