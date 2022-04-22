c56a5ab06f159195fb88a83423e956bd8c2123dd
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-21 18:17:53 -0700

    moves the stack list down under the repo and demo links
    also adds border around individual tech stack

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Project = ({ title, description, stack, repo, demo }) => {
   return (
     <Wrapper>
       <Title>{title} -</Title>
       <p>{description}</p>
-      <p>Tech Stack: {stack}</p>
-      <div>
-        <a href={repo}>view repo</a>
-        <a href={demo}>live demo</a>
-      </div>
+      <LinkWrapper>
+        <Link href={repo}>View Repo</Link>
+        <Link href={demo}>Live Demo</Link>
+      </LinkWrapper>
+      <Stack>
+        {stack.map((skill, index) => (
+          <Skill key={index}>{skill}</Skill>
+        ))}
+      </Stack>
     </Wrapper>
   )
 }
@@ -18,9 +23,35 @@ const Project = ({ title, description, stack, repo, demo }) => {
 const Wrapper = styled.div`
   display: flex;
   flex-flow: column;
+  gap: 16px;
 `
 const Title = styled.h3`
   font-size: 1.5rem;
+  color: hsl(0, 0%, 30%);
   /* width: 100%; */
+`
+const Stack = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  gap: 16px;
+  font-style: italic;
+`
+const Skill = styled.div`
+  border: 2px solid hsl(0, 0%, 25%);
+  border-radius: 16px;
+  padding: 4px 16px;
+`
+const LinkWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`
+const Link = styled(motion.a)`
+  display: block;
+  &:link,
+  &:visited {
+    color: hsl(0, 0%, 25%);
+    text-decoration: none;
+    cursor: pointer;
+  }
 `
 export default Project
