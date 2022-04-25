65226dceb80423830345290d63b85ea735ab56e6
Ruben Ramirez <rramirez110911@gmail.com>
2022-04-24 20:18:25 -0700

    adds a copy to clipboard object

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const Contact = () => {
+const Contact = ({ copied, setCopied }) => {
   return (
     <Wrapper>
       <Heading>Get In Touch</Heading>
-      <div>
-        <p>contact info here</p>
-      </div>
+      <CopyToClipboard
+        text='rubenramirez.dev@gmail.com'
+        onCopy={() => setCopied(!copied)}
+      >
+        <Email>
+          {/* <p>RUBENRAMIREZ.DEV@GMAIL.COM</p> */}
+          {copied ? <p>Copied!</p> : <p>rubenramirez.dev@gmail.com</p>}
+        </Email>
+      </CopyToClipboard>
     </Wrapper>
   )
 }
@@ -16,9 +24,14 @@ const Wrapper = styled.div`
   display: flex;
   flex-flow: column;
   gap: 16px;
+  width: 100%;
+  overflow-wrap: break-word;
 `
 const Heading = styled.h2`
   font-size: 1.7rem;
   width: 100%;
+`
+const Email = styled(motion.div)`
+  cursor: pointer;
 `
 export default Contact
