e72e864182a8e8fbd3367018b7b75d3200a6f785
Ruben Ramirez <rramirez110911@gmail.com>
2023-02-08 22:04:51 -0800

    grammer changes

--- src/components/Accordian.js
+++ src/components/Accordian.js
@@ -24,25 +24,24 @@ const Accordian = ({ isToggled, setIsToggled, hoverVariant }) => {
             exit={{ opacity: 0, height: 0 }}
           >
             <p>
-              My first coding experience was in Economics class my senior year
-              of high school. The Economics teacher said, while showing us
+              My first coding experience was in Economics class during my senior
+              year of high school. The Economics teacher said, while showing us
               Codeacademy, " These will be important skills to have in the
               future." And I continued to self-teach web development on and off
-              for a couple of years, until fully commiting to a coding bootcamp
-              LEARN academy. While there I built a solid foundation of web
-              development fundamentals and obtained a growth mindset.
+              for a couple of years until I fully committed to attend the coding
+              bootcamp LEARN academy. While there I built a solid foundation of
+              web development fundamentals and obtained a growth mindset.
             </p>
             <br />
             <p>
               I am now seeking opportunities where I can help a company achieve
-              their goals, while also improving my skills and knowledge as a
+              its goals, while also improving my skills and knowledge as a
               developer.
             </p>
             <br />
             <p>
               Lastly, when not coding I enjoy immersing myself in video games,
-              swimming, visiting the beach, and exploring new creative
-              interests.
+              swimming, inline skating, and exploring new creative interests.
             </p>
           </motion.article>
         )}
